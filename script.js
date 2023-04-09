a9b6f298d52ef00006db84f9ac365178ba8205af
pranjali24-k <127399894+pranjali24-k@users.noreply.github.com>
2023-04-10 00:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,12 +10,25 @@ const lookup = {
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return ;//return decodedArr
+function rot13(str) {
+    var codeArr = [], seq, final = "";
+    for (var i = 0; i < str.length; i++) {
+        uCode = str.charCodeAt([i]);
+        if ((uCode > 64 && uCode < 78) || (uCode > 96 && uCode < 110)) {
+            uCode += 13;
+            codeArr.push(uCode);
+        } else if ((uCode > 77 && uCode < 91) || (uCode > 109 && uCode < 123)){
+            uCode -= 13;
+            codeArr.push(uCode);
+        } else { codeArr.push(uCode); }
+    }
+    console.log(codeArr);
+    for (var j = 0; j < codeArr.length; j++) {
+        final += String.fromCharCode(codeArr[j]);
+    }
+    return final;
 }
+ 
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
